51fc68af734c8cb09cdcc04417cecdd1675f6827
samuel grahame <samuel@intelogy.com.au>
2016-09-22 14:48:37 +1000

    Added Comments

--- Test/File.cs
+++ Test/File.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace WinFormjs
 {
+	/// <summary>
+	/// Need to make File Interface. so that I can have DropBoxFile, LocalFile and FTPFile
+	/// </summary>
     public class File : FileExplorerNode
     {
         public static string ReadAllText(string path)
