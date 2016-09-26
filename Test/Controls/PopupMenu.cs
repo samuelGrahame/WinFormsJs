125e8e7761384e63ab4b3771143b692f2ff51c00
samfromdeath <samuel@intelogy.com.au>
2016-09-27 09:30:36 +1000

    Added PopupMenu Class - Not Finished

---
+++ Test/Controls/PopupMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormjs
+{
+    public class PopupMenu
+    {
+        public List<MenuItem> Items = new List<MenuItem>();
+        
+        public void Show()
+        {
+
+        }
+
+        public void Close()
+        {
+
+        }        
+    }
+
+    public class MenuItem
+    {
+        public string Caption { get; set; }
+        public bool Enabled { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class SubMenuItem : MenuItem
+    {
+        public PopupMenu Menu { get; set; }
+    }
+}
