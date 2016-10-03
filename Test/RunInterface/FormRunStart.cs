a77a0f8d34ea70407616dc4f7e690aeb0e14174e
samuel grahame <samuel@intelogy.com.au>
2016-10-03 15:03:36 +1100

    Run Server Applications

---
+++ Test/RunInterface/FormRunStart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge.Html5;
+
+namespace WinFormjs
+{
+	public class FormRunStart : Form
+	{
+		protected override void Initialise()
+		{
+			base.Initialise();
+
+			this.Text = "Run Interface";
+			this.Width = "400px";
+			this.Height = "100px";
+
+			var Input = new HTMLInputElement();
+			Input.Style.Position = Position.Absolute;
+			Input.Style.Left = "5px";
+			Input.Style.Top = "5px";
+
+			FillHorizontalControlWithParent(Input, 5);			
+
+			Input.OnKeyUp = (ev) => {
+				var kev = ev.As<KeyboardEvent>();
+				if(kev.KeyCode == 13)
+				{
+					if(string.IsNullOrWhiteSpace(Input.Value))
+					{
+						ServerApplication.StartFromURI(Input.Value);
+					}
+					Input.Value = "";
+				}
+			};
+
+
+			//FillHorizontalControlWithParent()
+
+
+
+			//this.Body.AppendChild()
+
+		}
+
+		protected override void OnShowing()
+		{
+			
+			base.OnShowing();
+		}
+	}
+}
---
+++ Test/RunInterface/ServerApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge.jQuery2;
+
+namespace WinFormjs
+{
+	public class ServerApplication
+	{
+		public string URI { get; set; }
+
+		public static void StartFromURI(string source)
+		{
+			// Get Application from Source :D
+		}
+	}
+}
